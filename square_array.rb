9d54a256f9f7a2ff38bbf08d9a14ecf64a1314b2
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-15 05:39:32 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,12 @@
 def square_array(array)
   arr = []
-  array.each do |numbers|
-    arr << numbers ** 2
+  array.each {|numbers|
+    arr << numbers ** 2}
   end
 end
 
-def square_array(array)
-  arr = []
-  array.each { |numbers| arr << numbers ** 2 }
-  arr
-end
+#def square_array(array)
+#  arr = []
+#  array.each { |numbers| arr << numbers ** 2 }
+#  arr
+#end
